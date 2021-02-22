d501ea5bd81708ffebcfcc649480ae6ef616680d
vetsinen <vetsinen@gmail.com>
2021-02-22 16:40:20 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'de1a89868bc0b43888823371d8110b0f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'feb845c9-5b64-40db-9175-43eea7a41757'
+};
